f257041e63acb605ce638b26ee1d5cbfe8c1316d
Isaiah Fischer <isaiah@singlebrook.com>
2013-06-19 13:32:35 -0500

    Add date begin and end to meetings

--- lib/adobe_connect/meeting.rb
+++ lib/adobe_connect/meeting.rb
@@ -2,10 +2,12 @@ module AdobeConnect
 
   # Public: Represents a meeting in a Connect environment.
   class Meeting < Base
-    attr_accessor :description, :folder_id, :name, :url_path
+    attr_accessor :date_begin, :date_end, :description, :folder_id, :name, :url_path
 
     #
     # meeting_options - A hash with the following keys:
+    #                date_begin  - (optional) Datetime that meeting begins.
+    #                date_end    - (optional) Datetime that meeting ends.
     #                description - A description of the meeting to be displayed to users.
     #                folder_id   - (Required for creating) ID of the meeting folder
     #                name        - Name of the meeting
@@ -13,10 +15,17 @@ module AdobeConnect
     #                              by AC if not provided
     #
 
+    [ :date_end, :date_begin ].each do |atr|
+      define_method atr do
+        val = self.instance_variable_get(:"@#{atr.to_s}")
+        val.utc.strftime('%Y-%m-%dT%H:%M:%SZ') unless val.nil?
+      end
+    end
+
     def attrs
       atrs = {}
 
-      [ :description, :name, :url_path ].each do |atr|
+      [ :date_end, :date_begin, :description, :name, :url_path ].each do |atr|
         atrs[atr] = send(atr)
       end
 
