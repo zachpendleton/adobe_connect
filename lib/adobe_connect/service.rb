f34656c567dc560401c1b6ad88a4015f2eb2fcae
Dermot Haughey <dermot.haughey@rvibe.com>
2013-11-04 16:53:31 -0600

    Add default

--- lib/adobe_connect/service.rb
+++ lib/adobe_connect/service.rb
@@ -84,6 +84,7 @@ module AdobeConnect
     #
     # Returns an AdobeConnect::Response.
     def request(action, params={}, use_session = true)
+      params ||={}
       if use_session
         log_in unless authenticated?
         params[:session] = session
