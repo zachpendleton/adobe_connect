16a5689076a20dfef2129b3b09b4209a0f188d8e
Leon Miller-Out <leon@singlebrook.com>
2016-01-07 10:22:18 -0500

    Fix deprecation warning in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ require File.expand_path('../lib/adobe_connect.rb', File.dirname(__FILE__))
 # Shared base tests for some objs
 require File.expand_path('lib/adobe_connect/adobe_connect_base_tests.rb', File.dirname(__FILE__))
 
-class AdobeConnectTestCase < MiniTest::Unit::TestCase
+class AdobeConnectTestCase < Minitest::Test
   def mock_ac_response(*args)
     resp = mock_response(*args)
     AdobeConnect::Response.new(resp)
